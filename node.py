d45c54bb13a48cbd572ef1ba34e157d4c6598544
Tianhao <ttao061@uottawa.ca>
2023-04-14 10:49:20 -0400

    update

---
+++ node.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self) -> None:
+        pass
+
+    def initTable(self):
+        return
+    
+    def initPort(self):
+        return
---
+++ packet.py
@@ -0,0 +1,7 @@
+class Packet:
+    def __init__(self, header):
+        self.header = header
+
+    def initHeader(self):
+        return
+    
